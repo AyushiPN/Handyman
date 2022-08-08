83062c2845bf24f63189594ed7b7a50e672168f8
Ayushi Nandeshwar <74812992+AyushiPN@users.noreply.github.com>
2022-08-08 23:46:15 +0530

    Updated Home

--- Handyman/src/components/Home/Home.js
+++ Handyman/src/components/Home/Home.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import Bookingimg from './Bookingimg.js'
 import Jumbotron from './Jumbotron.js'
-import Header from '../Header.js'
-import Footer from '../Footer.js'
-import Scarousel from './Scarousel.js'
+// import Header from '../Header.js'
+// import Footer from '../Footer.js'
+// import Scarousel from './Scarousel.js'
+import './Home.css';
+import Footer from '../Footer.js';
+import Header from "../Header";
 
 function Home() {
   return (
-    <div>
+    <div className='home container-fluid'>
       <Header />
       <Jumbotron />
       {/* <Scarousel /> */}
       <Bookingimg />
-      {/* <Footer /> */}
+      <Footer />
     </div>
     
   )
