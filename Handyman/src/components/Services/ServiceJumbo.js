c67c197db0aacd4447705437690cb8c02a157239
AyushiPN <ayushi.p.n@gmail.com>
2022-08-12 20:29:48 +0530

    Services updated

---
+++ Handyman/src/components/Services/ServiceJumbo.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './ServiceJumbo.css';
+// import Simage from '../../assets/sebg.png';
+
+function ServiceJumbo() {
+  return (
+    <div className="Simage"> 
+    
+      <div className="jumboText container">
+        <p className='headingJ'>Check various services provided by us</p>
+        
+      </div>
+    </div>
+  )
+}
+
+export default ServiceJumbo
